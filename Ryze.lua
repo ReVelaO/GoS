598eac3ba30d86f9c4c5bb3d5767c2c1d5b17b53
WeinerCH <drakateco@gmail.com>
2015-08-22 08:16:59 -0400

    Updated 5.16

--- Ryze.lua
+++ Ryze.lua
@@ -6,42 +6,53 @@ Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-PrintChat("<font color=\"#a375ff\"><b>[DarkRyze v1.0.1 By DarkFight] </b></font> <font color=\"#FFFFFF\">Loaded and Ready to use.</font>")
+PrintChat("<font color=\"#a375ff\"><b>[DarkRyze v1.1.2 By DarkFight] </b></font> <font color=\"#FFFFFF\">Loaded and Ready to use.</font>")
 
---5.15 Working.               
+--Reworked Combo, 5.16 Working.               
 local Cargas = "ryzepassivestack"
 local Pasiva = "ryzepassivecharged"
-
-
-				
-				
 
 OnLoop(function(myHero)
                 
         if IWalkConfig.Combo then
                    local target = GetTarget(1000, DAMAGE_MAGIC)
                         if ValidTarget(target, 1000) then
-                
+                		if (GotBuff(myHero, "ryzepassivestack") == 3) then
 						if CanUseSpell(myHero, _W) == READY and Config.W then
-						CastTargetSpell(target, _W)
+						CastTargetSpell(unit, _W)
 						end
                      	
-						local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,true,true)
-						local QWithWPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,false,true)		
-                        			if CanUseSpell(myHero, _Q) == READY and QWithWPred.HitChance == 1 and Config.Q and (GotBuff(myHero, "RyzeW") > 0) then
-                        			CastSkillShot(_Q,QWithWPred.PredPos.x,QWithWPred.PredPos.y,QWithWPred.PredPos.z)						
-						elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
-						CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-	                    			end
-
-						if CanUseSpell(myHero, _E) == READY and Config.E then
-						CastTargetSpell(target, _E)
+						local Q = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),900,250,GetCastRange(myHero,_Q),55,false,true)		
+                        			if CanUseSpell(myHero, _Q) == READY and Q.HitChance == 1 and Config.Q and (GotBuff(unit, "RyzeW") == 1) then
+                        			CastSkillShot(_Q,Q.PredPos.x,Q.PredPos.y,Q.PredPos.z)						
 						end
 
-						if CanUseSpell(myHero, _R) == READY and Config.W and (GotBuff(myHero, "ryzepassivecharged") > 0) and (GotBuff(target, "RyzeW") > 0) then
+						if CanUseSpell(myHero, _E) == READY and Config.E then
+						CastTargetSpell(unit, _E)
+						end
+
+						if CanUseSpell(myHero, _R) == READY and Config.R and (GotBuff(myHero, "ryzepassivecharged") == 1) and (GotBuff(unit, "RyzeW") == 1) then
 						CastSpell(_R)
 						end
 
+				else if (GotBuff(myHero, "ryzepassivestack") == 4) then
+						local Q = GetPredictionForPlayer(GetMyHeroPos(),unit,GetMoveSpeed(unit),900,250,GetCastRange(myHero,_Q),55,false,true)		
+                        			if CanUseSpell(myHero, _Q) == READY and Q.HitChance == 1 and Config.Q then
+                        			CastSkillShot(_Q,Q.PredPos.x,Q.PredPos.y,Q.PredPos.z)						
+						end
+						
+						if CanUseSpell(myHero, _W) == READY and Config.W then
+						CastTargetSpell(unit, _W)
+						end
+                     	
+						if CanUseSpell(myHero, _E) == READY and Config.E then
+						CastTargetSpell(unit, _E)
+						end
+
+						if CanUseSpell(myHero, _R) == READY and Config.R and (GotBuff(myHero, "ryzepassivecharged") == 1) and (GotBuff(unit, "RyzeW") == 1) then
+						CastSpell(_R)
+						end
 				end
+		end
 	end
 end)
