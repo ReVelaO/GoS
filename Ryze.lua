b92a5d9f3995cc6b8cf6f1636378aa6b253dceff
WeinerCH <drakateco@gmail.com>
2015-08-29 08:17:01 -0400

    [5.16] Updated for Dlib.

--- Ryze.lua
+++ Ryze.lua
@@ -1,20 +1,24 @@
-require ('Inspired')
-require ('IAC')                
-myIAC = IAC()
-Config = scriptConfig("Ryze", "DarkRyze")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("RR", "Use R if rooted", SCRIPT_PARAM_ONOFF, true)
-PrintChat("<font color=\"#00bfff\"><b>[DarkRyze v1.1.3 By DarkFight] </b></font> <font color=\"#FFFFFF\">Loaded and Ready to use.</font>")
+require('DLib')
 
+local root = menu.addItem(SubMenu.new("DarkRyze"))
+local Combo = root.addItem(SubMenu.new("Combo"))
+	local QU = Combo.addItem(MenuBool.new("Use Q",true))
+	local WU = Combo.addItem(MenuBool.new("Use W",true))
+	local EU = Combo.addItem(MenuBool.new("Use E",true))
+	local RU = Combo.addItem(MenuBool.new("Use R",true))
+	local RRU = Combo.addItem(MenuBool.new("Use R if rooted",true))
+	local ComboActive = Combo.addItem(MenuKeyBind.new("Combo", 32))
+local Misc = root.addItem(SubMenu.new("Misc"))
+	local ALP = Misc.addItem(MenuBool.new("Auto Level Spells",true))
 --Reworked Combo, 5.16 Working.               
 local Pasiva = "ryzepassivecharged"
 
 OnLoop(function(myHero)
-                
-if GetLevel(myHero) == 1 then
+
+local myHeroPos = GetOrigin(myHero)
+local target = GetCurrentTarget()
+
+if GetLevel(myHero) == 1 and ALP.getValue() then
 	LevelSpell(_Q)
 elseif GetLevel(myHero) == 2 then
 	LevelSpell(_W)
@@ -52,28 +56,71 @@ elseif GetLevel(myHero) == 18 then
         LevelSpell(_E)
 end
 
-if IWalkConfig.Combo then
-local target = GetTarget(1000, DAMAGE_MAGIC)
-if ValidTarget(target, 1000) then
-							
-if CanUseSpell(myHero, _W) == READY and Config.W then
-CastTargetSpell(target, _W)
-end
-                     	
-local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,false,true)		
-if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
-CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)						
-end
+if ComboActive.getValue() then 
+	if ValidTarget(target, 900) then					
+		if CanUseSpell(myHero, _W) == READY and QU.getValue() then
+			CastTargetSpell(target, _W)
+			end     
+                	
+		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,false,true)		
+		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and QU.getValue() then
+			CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)						
+			end
 
-if CanUseSpell(myHero, _E) == READY and Config.E then
-CastTargetSpell(target, _E)
-end
+		if CanUseSpell(myHero, _E) == READY and EU.getValue() then
+			CastTargetSpell(target, _E)
+			end
 
-if CanUseSpell(myHero, _R) == READY and Config.R and (GotBuff(myHero, "ryzepassivecharged") > 0) then
-CastSpell(_R)
-elseif CanUseSpell(myHero, _R) == READY and Config.RR and (GotBuff(myHero, "ryzepassivecharged") > 0) and (GotBuff(target , "RyzeW") == 1) then
-CastSpell(_R)
-end				
-end
-end
+		if CanUseSpell(myHero, _R) == READY and RU.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
+			CastSpell(_R)
+		elseif CanUseSpell(myHero, _R) == READY and RRU.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) and (GotBuff(target , "RyzeW") == 1) then
+			CastSpell(_R)
+			end
+		end
+	end
 end)
+
+function CalcDamage(source, target, addmg, apdmg)
+    local ADDmg = addmg or 0
+    local APDmg = apdmg or 0
+    local ArmorPen = math.floor(GetArmorPenFlat(source))
+    local ArmorPenPercent = math.floor(GetArmorPenPercent(source)*100)/100
+    local Armor = GetArmor(target)*ArmorPenPercent-ArmorPen
+    local ArmorPercent = Armor > 0 and math.floor(Armor*100/(100+Armor))/100 or math.ceil(Armor*100/(100-Armor))/100
+    local MagicPen = math.floor(GetMagicPenFlat(source))
+    local MagicPenPercent = math.floor(GetMagicPenPercent(source)*100)/100
+    local MagicArmor = GetMagicResist(target)*MagicPenPercent-MagicPen
+    local MagicArmorPercent = MagicArmor > 0 and math.floor(MagicArmor*100/(100+MagicArmor))/100 or math.ceil(MagicArmor*100/(100-MagicArmor))/100
+    return (GotBuff(source,"exhausted")  > 0 and 0.4 or 1) * math.floor(ADDmg*(1-ArmorPercent))+math.floor(APDmg*(1-MagicArmorPercent))
+end
+
+function GetEnemyHeroes()
+  local result = {}
+  for _, obj in pairs(objectManager.heroes) do
+    if GetTeam(obj) ~= GetTeam(GetMyHero()) then
+      table.insert(result, obj)
+    end
+  end
+  return result
+end
+
+function ValidTarget(unit, range)
+    range = range or 25000
+    if unit == nil or GetOrigin(unit) == nil or not IsTargetable(unit) or IsDead(unit) or not IsVisible(unit) or GetTeam(unit) == GetTeam(GetMyHero()) or not IsInDistance(unit, range) then return false end
+    return true
+end
+
+function IsInDistance(p1,r)
+    return GetDistanceSqr(GetOrigin(p1)) < r*r
+end
+
+function GetDistanceSqr(p1,p2)
+    p2 = p2 or GetMyHeroPos()
+    local dx = p1.x - p2.x
+    local dz = (p1.z or p1.y) - (p2.z or p2.y)
+    return dx*dx + dz*dz
+end
+
+function GetMyHeroPos()
+    return GetOrigin(GetMyHero()) 
+end
