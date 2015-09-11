05b83dace64e27c085d508418ee57239bd380f89
WeinerCH <drakateco@gmail.com>
2015-09-11 17:28:48 -0400

    don't support dlib

--- Ryze.lua
+++ Ryze.lua
@@ -1,4 +1,3 @@
-require('DLib')
 require('IOW')
 
 supportedHero = {["Ryze"] = true}
@@ -8,91 +7,89 @@ function Ryze:__init()
 
 OnLoop(function(myHero) self:Loop(myHero) end)
 
-root = menu.addItem(SubMenu.new("DarkRyze"))
-Combo = root.addItem(SubMenu.new("Combo"))
-QU = Combo.addItem(MenuBool.new("Use Q",true))
-WU = Combo.addItem(MenuBool.new("Use W",true))
-EU = Combo.addItem(MenuBool.new("Use E",true))
-RU = Combo.addItem(MenuBool.new("Use R",true))
-RRU = Combo.addItem(MenuBool.new("Use R if rooted",true))
-Comb = Combo.addItem(MenuKeyBind.new("Combo", 32))
+MainMenu = Menu("Ryze", "Ryze")
+MainMenu:SubMenu("Combo", "Combo")
+MainMenu.Combo:Boolean("Q", "Use Q", true)
+MainMenu.Combo:Boolean("W", "Use W", true)
+MainMenu.Combo:Boolean("E", "Use E", true)
+MainMenu.Combo:Boolean("R", "Use R", true)
+MainMenu.Combo:Boolean("RR", "Use R if rooted", true)
 	
-Farm = root.addItem(SubMenu.new("Farm"))
-LaneClear = Farm.addItem(SubMenu.new("Lane Clear"))
-useQ = LaneClear.addItem(MenuBool.new("Use Q",true))
-useW = LaneClear.addItem(MenuBool.new("Use W",true))
-useE = LaneClear.addItem(MenuBool.new("Use E",true))
-useR = LaneClear.addItem(MenuBool.new("Use R",false))
-LClear = LaneClear.addItem(MenuKeyBind.new("Lane Clear", 86))
-JungleClear = Farm.addItem(SubMenu.new("Jungle Cear"))
-JuseQ = JungleClear.addItem(MenuBool.new("Use Q",true))
-JuseW = JungleClear.addItem(MenuBool.new("Use W",true))
-JuseE = JungleClear.addItem(MenuBool.new("Use E",true))
-JuseR = JungleClear.addItem(MenuBool.new("Use R",false))
-JClear = JungleClear.addItem(MenuKeyBind.new("Jungle Clear", 86))
+MainMenu:SubMenu("LaneClear", "Lane Clear")
+MainMenu.LaneClear:Boolean("Q", "Use Q", true)
+MainMenu.LaneClear:Boolean("W", "Use W", true)
+MainMenu.LaneClear:Boolean("E", "Use E", true)
+MainMenu.LaneClear:Boolean("R", "Use R", false)
+
+MainMenu:SubMenu("JungleClear", "Jungle Clear")
+MainMenu.JungleClear:Boolean("Q", "Use Q", true)
+MainMenu.JungleClear:Boolean("W", "Use W", true)
+MainMenu.JungleClear:Boolean("E", "Use E", true)
+MainMenu.JungleClear:Boolean("R", "Use R", false)
+
+MainMenu:SubMenu("Misc", "Misc")
+MainMenu.Misc:Boolean("AutoLevelS", "Auto-Level Spells", true)
 	
-Misc = root.addItem(SubMenu.new("Misc"))
-ALS = Misc.addItem(MenuBool.new("Auto Level Spells",true))
-	
-Drawings = root.addItem(SubMenu.new("Drawings"))
-Enable = Drawings.addItem(MenuBool.new("Enable Drawings",true))
-DrawQ = Drawings.addItem(MenuBool.new("Draw Q Range",true))
-DrawWE = Drawings.addItem(MenuBool.new("Draw W + E Range",true))
-DrawHD = Drawings.addItem(MenuSlider.new("Quality Circles (High Number More FPS)", 255, 1, 255, 1))
-Info = Drawings.addItem(MenuSeparator.new("If Drawings has not purple color then"))
-Info1 = Drawings.addItem(MenuSeparator.new("Press F6 x2"))
+MainMenu:SubMenu("Drawings", "Drawings")
+MainMenu.Drawings:Boolean("ED", "Enable Drawings", true)
+MainMenu.Drawings:Boolean("DQ", "Draw Q Range", true)
+MainMenu.Drawings:Boolean("DWE", "Draw W + E Range", true)
+MainMenu.Drawings:Slider("DrawHD", "Quality Circles", 255, 1, 255, 1)
 	
 end
 --Updated 5.17.
 
 function Ryze:Loop(myHero)
-if _G.Comb.getValue() then 
+if _G.IOW:Mode() == "Combo" then 
 	self:DoCombo1()
+	end
+	
+if _G.IOW:Mode() == "Combo" then 
 	self:DoCombo2()
 	end
 
-if 	LClear.getValue() then
+if 	IOW:Mode() == "LaneClear" then
 	self:LaneClear()
 	end	
 	
-if 	JClear.getValue() then
+if 	IOW:Mode() == "JungleClear" then
 	self:JungleClear()
 	end		
 
-if	ALS.getValue() then
+if	MainMenu.Misc.AutoLevelS:Value() then
 	self:AutoLevelS()
 	end
 
-if 	Enable.getValue() then
+if 	MainMenu.Drawings.ED:Value() then
 	self:Drawings()
 	end
 end
 
 
 function Ryze:DoCombo1()
-if Comb.getValue() and GotBuff(myHero, "ryzepassivestacks") >= 2 then 
+if IOW:Mode() == "Combo" and GotBuff(myHero, "ryzepassivestacks") >= 2 then 
 	local target = IOW:GetTarget()
 			
 	if GoS:ValidTarget(target, 900) then					
-		if CanUseSpell(myHero, _W) == READY and WU.getValue() then
+		if CanUseSpell(myHero, _W) == READY and MainMenu.Combo.W:Value() then
 			CastTargetSpell(target, _W)
 			end                     	
 		
 		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,false,true)
 		local ChampEnemy = GetOrigin(target)		
-		if CanUseSpell(myHero, _Q) == READY and (GotBuff(target, "RyzeW") == 1) and QU.getValue() then
+		if CanUseSpell(myHero, _Q) == READY and (GotBuff(target, "RyzeW") == 1) and MainMenu.Combo.Q:Value() then
 			CastSkillShot(_Q,ChampEnemy.x,ChampEnemy.y,ChampEnemy.z)						
-		elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and not (GotBuff(target, "RyzeW") == 1) and QU.getValue() then
+		elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and not (GotBuff(target, "RyzeW") == 1) and MainMenu.Combo.Q:Value() then
 			CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)						
 			end
 
-		if CanUseSpell(myHero, _E) == READY and EU.getValue() then
+		if CanUseSpell(myHero, _E) == READY and MainMenu.Combo.E:Value() then
 			CastTargetSpell(target, _E)
 			end
 
-		if CanUseSpell(myHero, _R) == READY and RU.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
+		if CanUseSpell(myHero, _R) == READY and MainMenu.Combo.R:Value() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
 			CastSpell(_R)
-		elseif CanUseSpell(myHero, _R) == READY and RRU.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) and (GotBuff(target , "RyzeW") == 1) then
+		elseif CanUseSpell(myHero, _R) == READY and MainMenu.Combo.RR:Value() and (GotBuff(myHero, "ryzepassivecharged") > 0) and (GotBuff(target , "RyzeW") == 1) then
 			CastSpell(_R)
 			end
 		end
@@ -100,27 +97,27 @@ if Comb.getValue() and GotBuff(myHero, "ryzepassivestacks") >= 2 then
 end
 
 function Ryze:DoCombo2()
-if Comb.getValue() and GotBuff(myHero, "ryzepassivestacks") <= 1 then 
+if IOW:Mode() == "Combo" and GotBuff(myHero, "ryzepassivestacks") <= 1 then 
 	local target = IOW:GetTarget()
 			
 	if GoS:ValidTarget(target, 900) then							                     			
 		local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_Q),55,false,true)
 		local ChampEnemy = GetOrigin(target)		
-		if CanUseSpell(myHero, _Q) == READY and (GotBuff(target, "RyzeW") == 1) and QU.getValue() then
+		if CanUseSpell(myHero, _Q) == READY and (GotBuff(target, "RyzeW") == 1) and MainMenu.Combo.Q:Value() then
 			CastSkillShot(_Q,ChampEnemy.x,ChampEnemy.y,ChampEnemy.z)						
-		elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and not (GotBuff(target, "RyzeW") == 1) and QU.getValue() then
+		elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and not (GotBuff(target, "RyzeW") == 1) and MainMenu.Combo.Q:Value() then
 			CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)						
 			end
 
-		if CanUseSpell(myHero, _W) == READY and WU.getValue() then
+		if CanUseSpell(myHero, _W) == READY and MainMenu.Combo.W:Value() then
 			CastTargetSpell(target, _W)
 			end
 		
-		if CanUseSpell(myHero, _E) == READY and EU.getValue() then
+		if CanUseSpell(myHero, _E) == READY and MainMenu.Combo.E:Value() then
 			CastTargetSpell(target, _E)
 			end
 
-		if CanUseSpell(myHero, _R) == READY and RU.getValue() then
+		if CanUseSpell(myHero, _R) == READY and MainMenu.Combo.R:Value() then
 			CastSpell(_R)
 			end
 		end
@@ -128,53 +125,53 @@ if Comb.getValue() and GotBuff(myHero, "ryzepassivestacks") <= 1 then
 end
 
 function Ryze:LaneClear()
-if LClear.getValue() then      
+if IOW:Mode() == "LaneClear" then      
                 for i,minion in pairs(GoS:GetAllMinions(MINION_ENEMY)) do    
                         if GoS:IsInDistance(minion, 600) then
                         local PMinion = GetOrigin(minion)
-						if CanUseSpell(myHero, _W) == READY and useW.getValue() then
+						if CanUseSpell(myHero, _W) == READY and MainMenu.LaneClear.W:Value() then
 						CastTargetSpell(minion, _W)
 						end
 						
-						if CanUseSpell(myHero, _Q) == READY  and useQ.getValue() then
+						if CanUseSpell(myHero, _Q) == READY  and MainMenu.LaneClear.Q:Value() then
 						CastSkillShot(_Q,PMinion.x,PMinion.y,PMinion.z)
 						end		
 						
-						if CanUseSpell(myHero, _E) == READY and useE.getValue() then
+						if CanUseSpell(myHero, _E) == READY and MainMenu.LaneClear.E:Value() then
 						CastTargetSpell(minion, _E)
 						end 		
              
-						if CanUseSpell(myHero, _R) == READY and useR.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
+						if CanUseSpell(myHero, _R) == READY and MainMenu.LaneClear.R:Value() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
 						CastSpell(_R)			 
 						end
-          end
-       end
+					end
+		end
     end    
 end
 
 function Ryze:JungleClear()
-if JClear.getValue() then      
+if IOW:Mode() == "JungleClear" then      
                 for i,minion in pairs(GoS:GetAllMinions(MINION_JUNGLE)) do    
                         if GoS:IsInDistance(minion, 600) then
                         local PMinion = GetOrigin(minion)
-						if CanUseSpell(myHero, _W) == READY and JuseW.getValue() then
+						if CanUseSpell(myHero, _W) == READY and MainMenu.JungleClear.W:Value() then
 						CastTargetSpell(minion, _W)
 						end
 						
-						if CanUseSpell(myHero, _Q) == READY  and JuseQ.getValue() then
+						if CanUseSpell(myHero, _Q) == READY  and MainMenu.JungleClear.Q:Value() then
 						CastSkillShot(_Q,PMinion.x,PMinion.y,PMinion.z)
 						end		
 						
-						if CanUseSpell(myHero, _E) == READY and JuseE.getValue() then
+						if CanUseSpell(myHero, _E) == READY and MainMenu.JungleClear.E:Value() then
 						CastTargetSpell(minion, _E)
 						end 		
              
-						if CanUseSpell(myHero, _R) == READY and JuseR.getValue() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
+						if CanUseSpell(myHero, _R) == READY and MainMenu.JungleClear.R:Value() and (GotBuff(myHero, "ryzepassivecharged") > 0) then
 						CastSpell(_R)			 
 						end
           		end
        		end
-	end    
+		end    
 end
 
 function Ryze:AutoLevelS()
@@ -218,11 +215,11 @@ end
 end
 
 function Ryze:Drawings()
-if DrawQ.getValue() then
-	DrawCircle(GetOrigin(myHero),895,3,DrawHD.getValue(),0xff7B68EE)
+if MainMenu.Drawings.DQ:Value() then
+	DrawCircle(GetOrigin(myHero),895,3,MainMenu.Drawings.DrawHD:Value(),0xff7B68EE)
 	end
-if DrawWE.getValue() then
-	DrawCircle(GetOrigin(myHero),600,3,DrawHD.getValue(),0xff7B68EE)
+if MainMenu.Drawings.DWE:Value() then
+	DrawCircle(GetOrigin(myHero),600,3,MainMenu.Drawings.DrawHD:Value(),0xff7B68EE)
 	end
 end
 
